8fd34bb2808d5caa79edc25f8dfd111eb7bb84ec
Edward Rodriguez <luisrova17@gmail.com>
2018-03-06 03:09:15 -0600

    Logica de matriz

--- app/src/main/java/com/luis/edward/tetristareacorta2/MainActivity.java
+++ app/src/main/java/com/luis/edward/tetristareacorta2/MainActivity.java
@@ -4,6 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Integer matrizPantalla[][]=new Integer[20][20];
+
+    public void()
+    {
+        for (:             )
+        {
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
